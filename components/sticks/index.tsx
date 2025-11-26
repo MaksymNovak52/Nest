5b1afe821deb8e53b7ba194f6fe2ee3b217f593d
MaksKovsk <max@gigaver.se>
2025-11-26 13:43:34 +0200

    fix:auto play

--- components/sticks/index.tsx
+++ components/sticks/index.tsx
@@ -523,14 +523,14 @@ export function Sticks() {
     // check if user agent is safari and we have the ref to the container <div />
     if (isSafari() && appearVideoRef.current) {
       // obtain reference to the video element
-      const player = appearVideoRef.current.children[0];
+      const player = appearVideoRef.current.children[0] as HTMLVideoElement;
 
       // if the reference to video player has been obtained
       if (player) {
         // set the video attributes using javascript as per the
         // webkit Policy
         player.controls = false;
-        player.playsinline = true;
+        player.playsInline = true;
         player.muted = true;
         player.setAttribute("muted", ""); // leave no stones unturned :)
         player.autoplay = true;
@@ -540,12 +540,14 @@ export function Sticks() {
           // player.play() might return a promise but it's not guaranteed crossbrowser.
           const promise = player.play();
           // let's play safe to ensure that if we do have a promise
-          if (promise.then) {
+          if (promise && promise.then) {
             promise
               .then(() => {})
               .catch(() => {
                 // if promise fails, hide the video and fallback to <img> tag
-                appearVideoRef.current.style.display = "none";
+                if (appearVideoRef.current) {
+                  appearVideoRef.current.style.display = "none";
+                }
               });
           }
         }, 0);
