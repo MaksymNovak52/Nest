bc36aa18265c2007001eca64013c77dbff448bf4
MaksKovsk <max@gigaver.se>
2025-11-26 15:02:30 +0200

    fix:new screnn size

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import {
 export default function Home() {
   return (
     <main
-      className="max-w-[1636px] max-h-[896px] lg:min-h-auto  h-screen max-h- [906px] px-[20px] lg:px-[56px] box-border mx-auto relative overflow-hidden
+      className="max-w-[1636px]  lg:min-h-auto  h-screen max-h- [906px] px-[20px] lg:px-[56px] box-border mx-auto relative overflow-hidden
       "
     >
       {/* <video
