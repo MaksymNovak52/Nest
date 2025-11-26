1fb3d6428670b53f75ce9b25b36ea7f55a2436f5
MaksKovsk <max@gigaver.se>
2025-11-26 12:43:30 +0200

    feat:footer updates

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,5 +1,3 @@
-import { ENGINE_HYPE_STATS } from "@/constants";
-
 export function HeroFooter() {
   return (
     <footer className="lg:w-[428px]  w-[380px] mt-auto flex   absolute bottom-[104px] left-[40px] max-lg:left-1/2 max-lg:translate-x-[-50%] lg:bottom-[22px] z-10">
@@ -10,12 +8,25 @@ export function HeroFooter() {
         </span>
         <span className=" ">compounding $HYPE into $MEGAHYPE</span>
       </div>
-      <div className=" lg:flex flex-col gap-[21px] hidden ">
+      <div
+        className="relative left-[11px]   flex  gap-4 bottom-[100px]    text-white
+  font-host-grotesk
+  text-[12px]
+  font-medium
+  leading-[100%]
+  tracking-[0.36px]
+  uppercase
+  "
+      >
+        <span> + </span>
+        <span> +</span>
+      </div>
+      <div className=" lg:flex flex-col w-[240px] gap-[21px] hidden mb-2 ">
         <span className="font-medium text-[12px] text-[#6F7371] uppercase">
           <span className="text-[#FFFFFF]">Like Strategy₿ onchain.</span>{" "}
           Perpetual accumulation compounding $HYPE into $MEGAHYPE
         </span>
-        <div className="flex flex-col items-start gap-2 ">
+        {/* <div className="flex flex-col items-start gap-2 ">
           {ENGINE_HYPE_STATS.map((item) => (
             <div className="flex flex-row items-center gap-2" key={item.title}>
               <span className="font-bold text-[14px] text-[#CCCDCD]">
@@ -26,7 +37,7 @@ export function HeroFooter() {
               </span>
             </div>
           ))}
-        </div>
+        </div> */}
       </div>
     </footer>
   );
