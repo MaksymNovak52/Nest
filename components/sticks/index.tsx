9055bd71891373fe96c438d9e8e5c10ec0cab5b3
MaksKovsk <max@gigaver.se>
2025-11-26 22:59:33 +0200

    fix:video for mobile

--- components/sticks/index.tsx
+++ components/sticks/index.tsx
@@ -492,7 +492,7 @@ export function Sticks() {
         playsInline
         style={{ display: "block" }}
       >
-        <source src={"/video/AppearForMobile.mp4"} />
+        <source src={"/video/appear.mp4"} />
       </video>
 
       <video
@@ -503,7 +503,7 @@ export function Sticks() {
         loop
         style={{ display: "none" }}
       >
-        <source src={"/video/AppearForMobile.mp4"} />
+        <source src={"/video/loop.mp4"} />
       </video>
 
       {videoRect.width > 0 && (
